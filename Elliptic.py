76938d7ace95e6a268fbad069f1de01d6873c44c
Mark Coup <coupmark@gmail.com>
2016-12-13 14:48:00 -0700

    Cleaned up code

--- Elliptic.py
+++ Elliptic.py
@@ -26,37 +26,6 @@ fit1 = EllipticEnvelope(contamination=0.25).fit(X)
 fit2 = EllipticEnvelope(contamination=0.25).fit(x, y)
 
 
-def make_subplot(X, covs, ax, covariate=None, pcX=0,pcY=1,fontSize=10,fontName='sans serif',ms=20,leg=True,title=None):
-    colors = ['k','cyan','r','orange','g','b','magenta']
-    cvNames = np.sort(np.unique(covs[covariate]))
-    lines = []
-
-
-    for tick in ax.xaxis.get_major_ticks():
-        tick.label.set_fontsize(fontSize-2)
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label.set_fontsize(fontSize-2)
-
-    buff = 0.02
-    bufferX = buff * (fit[:,pcX].max() - fit[:,pcX].min())
-    bufferY = buff * (fit[:,pcY].max() - fit[:,pcY].min())
-    ax.set_xlim([fit[:,pcX].min()-bufferX,fit[:,pcX].max()+bufferX])
-    ax.set_ylim([fit[:,pcY].min()-bufferY,fit[:,pcY].max()+bufferY])
-    ax.set_xlabel("D-%s"%str(pcX+1),fontsize=fontSize,fontname=fontName)
-    ax.set_ylabel("D-%s"%str(pcY+1),fontsize=fontSize,fontname=fontName)
-    plt.locator_params(axis='x',nbins=5)
-    ax.set_aspect(1./ax.get_data_ratio())
-
-    if title:
-        ax.set_title(title,fontsize=fontSize+2,fontname=fontName)
-    if leg:
-        legend = ax.legend(lines,cvNames,loc='upper right',scatterpoints=1,
-                           handletextpad=0.01,labelspacing=0.01,borderpad=0.1,handlelength=1.0)
-
-        for label in legend.get_texts():
-            label.set_fontsize(fontSize-2)
-            label.set_fontname(fontName)
-
 
 def make_subplot_again(X, c, ax,pcX=0,pcY=1,fontSize=24,fontName='sans serif',ms=20,leg=True,title=None):
     outliers_fraction = 0.30
@@ -93,12 +62,6 @@ def make_subplot_again(X, c, ax,pcX=0,pcY=1,fontSize=24,fontName='sans serif',ms
     ax.contourf(xx, yy, Z, levels=[threshold, Z.max()],colors='orange')
     ax.axis('tight')
 
-    # cvNames = np.sort(np.unique(covs[covariate]))
-    # lines = []
-    # for _i,i in enumerate(cvNames):
-    #     indices = np.where(covs[covariate]==i)[0]
-    #     s = ax.scatter(X[indices,pcX],X[indices,pcY],c=colors[_i],s=ms,label=covariate,alpha=0.9)
-    #     lines.append(s)
     care_about = Xn[Xn['points_per_dollar'] > 3.5]
     care_about_false = care_about[care_about['pred'] == False]
 
@@ -142,38 +105,12 @@ def make_subplot_again(X, c, ax,pcX=0,pcY=1,fontSize=24,fontName='sans serif',ms
 
     if title:
         ax.set_title(title,fontsize=fontSize+2,fontname=fontName)
-    # if leg:
-    #     legend = ax.legend(lines,cvNames,loc='upper right',scatterpoints=1,
-    #                        handletextpad=0.01,labelspacing=0.01,borderpad=0.1,handlelength=1.0)
 
-        # for label in legend.get_texts():
-        #     label.set_fontsize(fontSize-2)
-        #     label.set_fontname(fontName)
-
-## variables
 if __name__ == '__main__':
     fig = plt.figure(figsize=(12, 8))
     ax1 = fig.add_subplot(111)
     make_subplot_again(X, c_x, ax1, leg=False, title='Quarterback Points')
     plt.axes().set_aspect(1/ax1.get_data_ratio())
     plt.show()
-#
-# ## make the figure again
-# plt.clf()
-# fig = plt.figure()
-# ax1 = fig.add_subplot(221)
-# ax2 = fig.add_subplot(222)
-# ax3 = fig.add_subplot(223)
-# ax4 = fig.add_subplot(224)
-# make_subplot_again(fit1,covs,'phenotype',ax1,pcX=0,pcY=1,leg=True,title='PCA-raw')
-# make_subplot_again(fit2,covs,'phenotype',ax2,pcX=0,pcY=1,leg=False,title='tSNE-raw')
-# make_subplot_again(fit3,covs,'phenotype',ax3,pcX=0,pcY=1,leg=False,title='PCA-scaled')
-# make_subplot_again(fit4,covs,'phenotype',ax4,pcX=0,pcY=1,leg=False,title='tSNE-scaled')
-# ax1.set_xlabel("")
-# ax2.set_xlabel("")
-# ax2.set_ylabel("")
-# ax4.set_ylabel("")
-# plt.subplots_adjust(hspace=0.3,wspace=0.05)
-# plt.savefig("outliers-detection.png",dpi=600)
     outlier = df[df['points_per_dollar'] > 8]
     bad_outlier = df[(df['points_per_dollar'] < 2) & (df['DK salary'] > 8000)]
