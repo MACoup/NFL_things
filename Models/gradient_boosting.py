62ba8c8311b40dbdc73ae53b9c3c7a81b4f33a20
Mark Coup <coupmark@gmail.com>
2017-02-27 16:03:52 -0700

    Running new model

--- Models/gradient_boosting.py
+++ Models/gradient_boosting.py
@@ -7,30 +7,84 @@ from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_sc
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import GradientBoostingClassifier as gbc
 from sklearn.metrics import mean_squared_error, accuracy_score
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 
 
 
-drop_cols = ['season_year', 'season_type', 'week', 'full_name', 'position', 'DK points', 'team', 'opp_team']
+
 
 
 # starting with QB
+# without scale
 
-fin = FinalDF(position='QB')
-df_qb = fin.get_df()
 
-df_qb.drop(drop_cols, axis=1, inplace=True)
+def get_feature_matrix(position, drop_cols):
 
-cats_calls = ['h/a']
+    '''
+    INPUT: Desired position matrix, columns to drop.
+    OUTPUT: X, Y
+    '''
 
-y = df_qb.pop('points_bin')
-x = df_qb
+    fin = FinalDF(position=position)
+    df_qb = fin.get_df()
 
-X_train, X_test, y_train, y_test = train_test_split(x, y)
+    df_qb.drop(drop_cols, axis=1, inplace=True)
 
-clf = gbc()
 
-clf.fit(X_train, y_train)
+    y = df_qb.pop('points_bin')
+    x = df_qb
 
-y_pred = clf.predict(X_test)
+    return x, y
+
+
+def raw_data_model(x, y):
+
+    '''
+    INPUT: x, y
+    OUTPUT: Classifier, predicted y
+    '''
+
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+    clf = gbc()
+
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+
+    return clf, y_pred
+
+
+def grid_search(classifier, parameters, x, y):
+
+    gs = GridSearchCV(classifier, parameters, cv=5)
+
+    gs.fit(x, y)
+
+    return gs
+
+
+
+
+
+if __name__ == '__main__':
+
+    drop_cols = ['season_year', 'season_type', 'week', 'full_name', 'position', 'DK points', 'team', 'opp_team']
+
+    x, y = get_feature_matrix('QB', drop_cols)
+
+    clf = gbc()
+
+    X_train, X_test, y_train, y_test = train_test_split(x, y)
+
+    parameters = {'max_features': [None, 'auto', 'log2'],  'learning_rate': [0.1, 0.2, 0.3, 0.4, 0.5]}
+
+    gs = grid_search(clf, parameters, X_train, y_train)
+
+    In [145]: gs.best_score_
+Out[145]: 0.59257582616568583
+
+In [146]: gs.best_params_
+Out[146]: {'learning_rate': 0.1, 'max_features': 'log2'}
